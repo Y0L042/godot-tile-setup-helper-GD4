ffd86b9e55a95b5df011bbb0e66c8ed383264590
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-11-01 14:28:03 +0200

    QS

--- addons/tile_setup_helper/plugin.gd
+++ addons/tile_setup_helper/plugin.gd
@@ -1,26 +1,26 @@
-tool
+@tool
 extends EditorPlugin
 
 var plugin
 func _enter_tree():
-    plugin = preload("helper.gd").new()
-    add_inspector_plugin(plugin)
+	plugin = preload("helper.gd").new()
+	add_inspector_plugin(plugin)
 
 func _process(delta):
-    if plugin:
-        plugin.interface = get_editor_interface()
+	if plugin:
+		plugin.interface = get_editor_interface()
 
 func handles(object : Object):
-    if object is TileSet:
-        return true
+	if object is TileSet:
+		return true
 
 func edit(object : Object):
-    if plugin:
-        plugin.current = object
+	if plugin:
+		plugin.current = object
 
 func clear():
-    if plugin:
-        plugin.current = null
+	if plugin:
+		plugin.current = null
 
 func _exit_tree():
-    remove_inspector_plugin(plugin)
+	remove_inspector_plugin(plugin)
